7427e3a8c1a406a107950cb6480f47b84cecba42
Michael Malone <michael.j.b.malone@gmail.com>
2019-02-12 21:28:56 +1300

    Add a led reaction to button press for temp

--- logger.py
+++ logger.py
@@ -1,9 +1,34 @@
 import time
 import json
 
+from gpiozero import Button, RGBLED
 from BME280 import BME280
 from TSL2561 import TSL2561
 from temp_repo import TemperatureRepository
+
+led = RGBLED(20, 21, 22)
+
+def button_press():
+  temp = temp_reader.read()
+  colour = (0, 0, 0)
+  if temp.temperature > 30:
+    colour = (1, 0, 0) # red
+  elif temp.temperature > 25:
+    colour = (1, 0.5, 0) # orange
+  elif temp.temperature > 20:
+    colour = (1, 1, 0) # yellow
+  elif temp.temperature > 15:
+    colour = (0.5, 1, 0) # lime
+  elif temp.temperature > 10:
+    colour = (0, 1, 0) # green
+  elif temp.temperature > 5:
+    colour = (0, 1, 0.5) # cyan
+  else:
+    colour = (0, 0, 1) # blue
+  led.color = colour
+  time.sleep(1)
+  led.off()
+
 
 with open('db_config.json') as inf:
   dbconfig = json.load(inf)
@@ -12,10 +37,12 @@ repo = TemperatureRepository(**dbconfig)
 
 temp_reader = BME280()
 lux_reader = TSL2561()
+button = Button(4)
+button.when_pressed = button_press
 
 while True:
   temp = temp_reader.read()
   repo.insert_temp(temp)
   lux = lux_reader.read()
   repo.insert_lux(lux)
-  time.sleep(300)
+  button.wait_for_press(300)
